06e116ae333a0bbe0b3240dadad9a6ca01d46528
eliciane <elicianems@gmail.com>
2024-11-14 11:36:13 -0300

    Versao 15

--- main.py
+++ main.py
@@ -21,12 +21,13 @@ df = st.dataframe(pd.read_csv('dados/Mapeamento_Competencias_08nov.csv'))
 
 
 # Skills Analytics Prep
-#df_equipe = df[['ID', 'Squad']].copy() # colunas ID e equipe
-#df_analy = df[['Analy_ PREP _Alteryx', 'Analy_ PREP _SAS','Analy_ PREP _Pentaho Data Integration', 'Analy_ PREP _Power Center','Analy_ PREP _SAP Analytics Cloud','Analy_ PREP _Oracle Data Integration','Analy_ PREP _SQL Server Integration Services']].copy()
+df_equipe = df[['ID', 'Squad']].copy() # colunas ID e equipe
+df_analy = df[['Analy_ PREP _Alteryx', 'Analy_ PREP _SAS','Analy_ PREP _Pentaho Data Integration', 'Analy_ PREP _Power Center','Analy_ PREP _SAP Analytics Cloud','Analy_ PREP _Oracle Data Integration','Analy_ PREP _SQL Server Integration Services']].copy()
 
 #df_analy = df.iloc[:,19:26] # colunas BI
 #df_concat_analy = pd.concat([df_equipe, df_analy], axis = 1)
-#print(df_concat_analy.head(4)) # imprime somente as quatro primeiras linhas
+
+
 
 #df_equipe = df_equipe.to_csv('/home/eliciane/PycharmProjects/people-analytics-five/dados/df_equipe.csv', index=False)
 #df_analy = df_analy.to_csv('/home/eliciane/PycharmProjects/people-analytics-five/dados/df_analy.csv', index=False)
@@ -34,10 +35,12 @@ df = st.dataframe(pd.read_csv('dados/Mapeamento_Competencias_08nov.csv'))
 #df_equipe= pd.read_csv('/home/eliciane/PycharmProjects/people-analytics-five/dados/df_equipe.csv')
 #df_analy = pd.read_csv('/home/eliciane/PycharmProjects/people-analytics-five/dados/df_analy.csv')
 
-df_equipe = st.dataframe(pd.read_csv('/dados/df_equipe.csv'))
-df_analy = st.dataframe(pd.read_csv('/dados/df_analy.csv'))
+#df_equipe = st.dataframe(pd.read_csv('/dados/df_equipe.csv'))
+#df_analy = st.dataframe(pd.read_csv('/dados/df_analy.csv'))
 
 df_concat_analy = pd.concat([df_equipe, df_analy], axis = 1)
+print(df_concat_analy.head(4)) # imprime somente as quatro primeiras linhas
+print(df_concat_analy.tail(4)) # imprime somente as quatro primeiras linhas
 
 st.subheader("Dados: Skills Analytics Prep")
 st.dataframe(df_concat_analy)
